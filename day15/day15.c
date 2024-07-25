8e62e861df7a553b35ea9d4cc00f2d631b41a79e
vikySeeker <vikyseeker@gmail.com>
2024-07-25 10:20:29 +0530

    day 15 assignment

---
+++ day15/day15.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+	int var = 69;
+	int *p_var = &var;
+	printf("The value of int variable is: %d\n", var);
+	printf("The value of the pointer to the int variable is: %d\n", p_var);
+	printf("The memory address of the int variable is: %d\n", var);
+	printf("The memory address of the pointer variable is: %d\n", &p_var);
+	printf("The value held at the memory location that the pointer is pointing to is: %d\n", var);
+	return 0;
+}
